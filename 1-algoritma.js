27821b4392eecdbae0e4704261b6962863481d4e
Restu Pamuji <100742235+restu2006576@users.noreply.github.com>
2022-04-05 09:02:28 +0700

    Add files via upload

---
+++ 1-algoritma.js
@@ -0,0 +1,21 @@
+// Buatlah algoritma menghitung luas persegi panjang
+// Input: panjang (p) dan lebar (1)
+// Luas (L) = p * 1
+
+// Algoritme
+
+
+```
+1. Masukan panjang dari persegi panjang (P)
+2. Masukan lebar dari persegi panjang(1)
+3. Menghitung luas persegin panjang dengan rumus L = p*1
+4. Menampilkan luas dari persegi panjang
+
+DEKLARASI
+panjang(P), lebar(1), Luas:integer
+
+ALGORITME
+Read(p,1)
+Luas (p*1)
+Write(luas)
+```
