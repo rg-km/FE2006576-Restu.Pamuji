85a9e7a88ff63031fb97a8379323b69aa056e4a7
restu2006576 <restupamoedji@gmail.com>
2022-04-11 12:43:34 +0700

    initial commit

--- frontend/javascript/hackerrank/diagonal-difference/main.js
+++ frontend/javascript/hackerrank/diagonal-difference/main.js
@@ -3,17 +3,31 @@
 // - Difficulty: Easy
 
 /*
-* Complete the 'diagonalDifference' function below.
-*
-* The function is expected to return an INTEGER.
-* The function accepts 2D_INTEGER_ARRAY arr as parameter.
-*/
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
 
 // Full Problem: https://www.hackerrank.com/challenges/diagonal-difference/problem
 
 function diagonalDifference(arr) {
   // Write your code here
   // TODO: answer here
+  let n = arr.length,
+    diag1 = 0,
+    diag2 = 0;
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      if (i === j) {
+        diag1 += arr[i][j];
+      }
+      if (i + j === n - 1) {
+        diag2 += arr[i][j];
+      }
+    }
+  }
+  return Math.abs(diag1 - diag2);
 }
 
 function main() {
@@ -21,7 +35,7 @@ function main() {
   var a = [
     [11, 2, 4],
     [4, 5, 6],
-    [10, 8, -12]
+    [10, 8, -12],
   ]; // override input
   let result = diagonalDifference(a);
   console.log(result);
@@ -29,4 +43,5 @@ function main() {
 
 main(); // execute program
 
-module.exports = diagonalDifference
+module.exports = diagonalDifference;
+
