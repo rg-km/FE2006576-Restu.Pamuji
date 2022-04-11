ebf63b7dfaf3b01f5cfaf03429e44ab3b00c2a18
restu2006576 <restupamoedji@gmail.com>
2022-04-11 13:01:35 +0700

    initial commit

--- frontend/javascript/hackerrank/time-conversion/main.js
+++ frontend/javascript/hackerrank/time-conversion/main.js
@@ -9,22 +9,35 @@
  * The function accepts STRING s as parameter.
  */
 
-
 // Full Problem: https://www.hackerrank.com/challenges/time-conversion/problem
-
 function timeConversion(s) {
   // Write your code here
   // TODO: answer here
+  let time = s.split(":");
+  let hour = parseInt(time[0]);
+  let minute = time[1];
+  let second = time[2].slice(0, 2);
+  let ampm = time[2].slice(2);
+  if (ampm === "PM") {
+    if (hour !== 12) {
+      hour += 12;
+    }
+  }
+  if (ampm === "AM") {
+    if (hour === 12) {
+      hour = 0;
+    }
+  }
+  return `${hour.toString().padStart(2, "0")}:${minute}:${second}`;
 }
 
 function main() {
   //var s = readLine();
-  var s = '07:05:45PM'; // override input
+  var s = "07:05:45PM"; // override input
   var result = timeConversion(s);
   console.log(result);
-
 }
 
 main(); // execute program
 
-module.exports = timeConversion
+module.exports = timeConversion;
