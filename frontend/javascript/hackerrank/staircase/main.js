db5b8ac8dbc26bc117bd8b93fd7871a4aeabf522
restu2006576 <restupamoedji@gmail.com>
2022-04-11 12:56:17 +0700

    initial commit

--- frontend/javascript/hackerrank/staircase/main.js
+++ frontend/javascript/hackerrank/staircase/main.js
@@ -9,19 +9,35 @@
  */
 
 // Full Problem: https://www.hackerrank.com/challenges/staircase/problem
+// Staircase
+// - Problem Solving (Basic)
+// - Difficulty: Easy
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+// Full Problem: https://www.hackerrank.com/challenges/staircase/problem
 
 function staircase(n) {
   // Write your code here
   // TODO: answer here
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += " ".repeat(n - i - 1) + "#".repeat(i + 1) + "\n";
+  }
+  return result;
 }
 
 function main() {
-  const n = 6
+  const n = 6;
 
   let result = staircase(n);
-  console.log(result)
+  console.log(result);
 }
 
 main(); // execute program
 
-module.exports = staircase
+module.exports = staircase;
