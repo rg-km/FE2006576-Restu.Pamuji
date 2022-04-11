9bed607161abfb7760e6fc8005ac1be0ad79f645
restu2006576 <restupamoedji@gmail.com>
2022-04-11 12:31:44 +0700

    initial commit

--- frontend/javascript/exercise/sandi-cp/main.js
+++ frontend/javascript/exercise/sandi-cp/main.js
@@ -18,15 +18,13 @@ terjemahKataSandi = "PASUKAN 1, MAJU KE AREA 2, JAM 3 SORE"
 Lengkapilah function dengan input kata sandi dan output terjemahannya
 */
 
-
 function terjemahKataSandi(kataSandi) {
   // TODO: answer here
   var terjemahannya = kataSandi.replace(/ /g, "E").replace(/\+/g, "A").replace(/&/g, "").replace(/%/g, "").replace(/\^/g, "").replace(/#/g, " ").replace(/]/g, ",");
   return terjemahannya;
 }
 
-
 console.log(terjemahKataSandi("&P%&+^S&^U&K+%N#1]#M&^+J^%%U#K #+^R&& +#2]#J+M#3#S^%%O&^R #"));
 // PASUKAN 1, MAJU KE AREA 2, JAM 3 SORE
 
-module.exports = terjemahKataSandi
+module.exports = terjemahKataSandi;
