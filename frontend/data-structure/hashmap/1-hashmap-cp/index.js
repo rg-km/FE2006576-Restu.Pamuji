b4a7cf1af4a08e879bb828b8483e78d2af5558fe
restu2006576 <restupamoedji@gmail.com>
2022-04-24 17:27:20 +0700

    initial commit

--- frontend/data-structure/hashmap/1-hashmap-cp/index.js
+++ frontend/data-structure/hashmap/1-hashmap-cp/index.js
@@ -1,26 +1,43 @@
 function ageDistribution(persons) {
     let result = new Map();
     // TODO: answer here
-    return result
-}
-
-function groupByAge(persons) {
+    for (let i = 0; i < persons.length; i++) {
+      let age = persons[i].age;
+      if (result.has(age)) {
+        result.set(age, result.get(age) + 1);
+      } else {
+        result.set(age, 1);
+      }
+    }
+    return result;
+  }
+  
+  function groupByAge(persons) {
     let result = new Map();
     // TODO: answer here
-    return result
-}
-
-let people = [
+    for (let i = 0; i < persons.length; i++) {
+      let age = persons[i].age;
+      if (result.has(age)) {
+        result.get(age).push(persons[i]);
+      } else {
+        result.set(age, [persons[i]]);
+      }
+    }
+    return result;
+  }
+  
+  let people = [
     { name: "Bob", age: 21 },
     { name: "Sam", age: 28 },
     { name: "Ann", age: 21 },
     { name: "Joe", age: 22 },
     { name: "Ben", age: 28 },
-]
-
-console.log(ageDistribution(people));
-console.log(groupByAge(people));
-
-module.exports = {
-    ageDistribution, groupByAge
-}
+  ];
+  
+  console.log(ageDistribution(people));
+  console.log(groupByAge(people));
+  
+  module.exports = {
+    ageDistribution,
+    groupByAge,
+  };
