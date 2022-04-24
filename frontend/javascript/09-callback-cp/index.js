5a1945b68fb46853ce440732168e559996209931
restu2006576 <restupamoedji@gmail.com>
2022-04-24 17:40:48 +0700

    initial commit

--- frontend/javascript/09-callback-cp/index.js
+++ frontend/javascript/09-callback-cp/index.js
@@ -4,16 +4,27 @@
 // contoh: hehe => hheehhee
 function doubleChars(str) {
   // TODO: answer here
+  let newString = "";
+  for (let i = 0; i < str.length; i++) {
+    newString += str[i] + str[i];
+  }
+  return newString;
 }
 
 // function ini mengulang pengaplikasian callback pada str sejumlah num
 function repeat(str, num, cb) {
   // TODO: answer here
+  let oldStr = cb(str);
+  let newStr = "";
+  for (let i = 0; i < oldStr.length; i++) {
+    newStr += oldStr[i].repeat(num);
+  }
+  return newStr;
 }
 
 console.log(repeat("triple", 2, doubleChars)); //  ttttrrrriiiipppplllleeee
 
 module.exports = {
   doubleChars,
-  repeat
-}
+  repeat,
+};
