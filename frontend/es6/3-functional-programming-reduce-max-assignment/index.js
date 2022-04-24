8f45382709a8ff82257ae426a9f2300738cd9807
restu2006576 <restupamoedji@gmail.com>
2022-04-24 15:10:53 +0700

    initial commit

--- frontend/es6/3-functional-programming-reduce-max-assignment/index.js
+++ frontend/es6/3-functional-programming-reduce-max-assignment/index.js
@@ -9,6 +9,14 @@
 
 const maxFromArray = (numbers) => {
   // TODO: answer here
+  let max = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    } else {
+      max = max;
+    }
+  }
   return max;
 };
 
