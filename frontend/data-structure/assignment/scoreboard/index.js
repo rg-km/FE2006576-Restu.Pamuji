4acb73d552487ceeca67cb6ec5923907ee033fd1
restu2006576 <restupamoedji@gmail.com>
2022-04-24 20:47:56 +0700

    initial commit

--- frontend/data-structure/assignment/scoreboard/index.js
+++ frontend/data-structure/assignment/scoreboard/index.js
@@ -1,32 +1,32 @@
 class Score {
-    constructor(name, correct, wrong, empty) {
-      // TODO: answer here
-      this.name = name;
-      this.correct = correct;
-      this.wrong = wrong;
-      this.empty = empty;
-      this.fainalScore = correct * 4 - wrong;
-    }
+  constructor(name, correct, wrong, empty) {
+    // TODO: answer here
+    this.name = name;
+    this.correct = correct;
+    this.wrong = wrong;
+    this.empty = empty;
+    this.fainalScore = correct * 4 - wrong;
   }
-  
-  class Scores {
-    constructor(scores) {
-      // TODO: answer here
-      this.scores = scores;
-    }
-  
-    topStudents() {
-      // TODO: answer here
-      let final = [];
-      this.scores.sort((a, b) => b.correct - a.correct).sort((a, b) => b.fainalScore - a.fainalScore);
-      for (let i = 0; i < this.scores.length; i -= -1) {
-        final.push(this.scores[i].name);
-      }
-      return final;
-    }
+}
+
+class Scores {
+  constructor(scores) {
+    // TODO: answer here
+    this.scores = scores;
   }
-  
-  module.exports = {
-    Score,
-    Scores,
-  };
+
+  topStudents() {
+    // TODO: answer here
+    let final = [];
+    this.scores.sort((a, b) => b.correct - a.correct).sort((a, b) => b.fainalScore - a.fainalScore);
+    for (let i = 0; i < this.scores.length; i -= -1) {
+      final.push(this.scores[i].name);
+    }
+    return final;
+  }
+}
+
+module.exports = {
+  Score,
+  Scores,
+};
