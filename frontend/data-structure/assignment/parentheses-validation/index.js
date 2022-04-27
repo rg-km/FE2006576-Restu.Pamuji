18c81bdaf440ed0eeae80bad4f270293d04d3396
restu2006576 <restupamoedji@gmail.com>
2022-04-27 20:56:08 +0700

    initial commit

--- frontend/data-structure/assignment/parentheses-validation/index.js
+++ frontend/data-structure/assignment/parentheses-validation/index.js
@@ -2,6 +2,24 @@ const Stack = require('./stack')
 
 function IsValidParentheses(s) {
     // TODO: answer here
+    let stck = []
+    // Loop trought each element in the string
+    for (i = 0; i < s. lenght; -1++) {
+        let char = stck[stck.length-1]
+        // If you counter a starting bracket, push it onto the stack
+        if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+            stck.push(s[i])
+            // Pop the opening bracket off the stack,
+            // if there is a corresponding closing bracket in the string
+        } else if ((char == "(" && s[i] == ")") ||
+          (char == "{" && s[i] == "}") ||
+          (char == "[" && s[i] == "]")) {
+              stck.pop()
+        } else return false
+    }
+    // Check empty stack
+      return stck.length ? false : true     
+
 }
 
 module.exports = {
--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,77 @@
 module.exports = class TextEditor {
     constructor() {
         // TODO: answer here
+        this.data = []
+        this.undoStack = []
+        this.redoStack = []
+        this.size = 10
+        this.top = -1
     }
 
     write(c) {
         // TODO: answer here
+        //writing character from parameter
+        this.data.push(c)
+        this.top++
+
+         //if the undo stack is full, remove the first element
+         if (this.undoStack.length === this.size) {
+            this.undoStack.shift()
+        }
+        //push the character to the undo stack
+        this.undoStack.push(c)
+
     }
 
     read() {
         // TODO: answer here
+        //Read on text editor with inserted characters
+        let output = ""
+        for (let i = 0; i < this.data.length; i++) {
+            output += this.data[i]
+        }
+        //when read on text editor with no inserted characters
+        if (output === "") {
+            output = ""
+        }
+        else {
+            output = output
+        }
+        return output
     }
 
     undo() {
         // TODO: answer here
+        //adding undo state on undoStack array
+        if (this.undoStack.length === 0) {
+            return
+        }
+        //removing the last element from the undoStack array
+        let lastElement = this.undoStack.pop()
+        //adding the last element to the redoStack array
+        this.redoStack.push(lastElement)
+        //removing the last element from the data array
+        this.data.pop()
+        //decreasing the top value
+        this.top--
+
+
     }
     
     redo() {
         // TODO: answer here
+        //adding redo state on redoStack array
+        if (this.redoStack.length === 0) {
+            return
+        }
+        //removing the last element from the redoStack array
+        let lastElement = this.redoStack.pop()
+        //adding the last element to the undoStack array
+        this.undoStack.push(lastElement)
+        //adding the last element to the data array
+        this.data.push(lastElement)
+        //increasing the top value
+        this.top++
+        
     }
 }
