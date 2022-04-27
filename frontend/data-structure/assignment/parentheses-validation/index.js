872b9cfe017f4ac231b775079bb377c65fcbfde2
restu2006576 <restupamoedji@gmail.com>
2022-04-27 21:01:19 +0700

    initial commit

--- frontend/data-structure/assignment/parentheses-validation/index.js
+++ frontend/data-structure/assignment/parentheses-validation/index.js
@@ -2,24 +2,30 @@ const Stack = require('./stack')
 
 function IsValidParentheses(s) {
     // TODO: answer here
-    let stck = []
-    // Loop trought each element in the string
-    for (i = 0; i < s. lenght; -1++) {
-        let char = stck[stck.length-1]
-        // If you counter a starting bracket, push it onto the stack
-        if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
-            stck.push(s[i])
-            // Pop the opening bracket off the stack,
-            // if there is a corresponding closing bracket in the string
-        } else if ((char == "(" && s[i] == ")") ||
-          (char == "{" && s[i] == "}") ||
-          (char == "[" && s[i] == "]")) {
-              stck.pop()
-        } else return false
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
     }
-    // Check empty stack
-      return stck.length ? false : true     
-
+    if (s.length === 0) {
+      return false;
+    }
+    if (s.length % 2 !== 0) {
+        return false;
+    }
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (char in map) {
+            const top = stack.pop();
+            if (top !== map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 }
 
 module.exports = {
