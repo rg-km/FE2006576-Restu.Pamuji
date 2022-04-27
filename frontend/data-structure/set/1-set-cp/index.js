bfbb168082704ed43370b704c76b99db47602fc2
restu2006576 <restupamoedji@gmail.com>
2022-04-27 11:01:47 +0700

    initial commit

--- frontend/data-structure/set/1-set-cp/index.js
+++ frontend/data-structure/set/1-set-cp/index.js
@@ -11,18 +11,23 @@
 // Output: {}
 // Explanation: tidak ada intersection dari a dan b
 
-
 function intersection(setA, setB) {
     let result = new Set();
+    for (let select of setA) {
+      if (setB.has(select)) {
+        result.add(select);
+      }
+    }
+  
     return result; // TODO: replace this
-}
-
-let setA = new Set(["Java", "Python", "Javascript", "C ++", "C#"]);
-let setB = new Set(["Java", "Python"]);
-console.log(intersection(setA, setB)); // {"Java", "Python"}
-
-setA = new Set("Java", "Python");
-setB = new Set("Swift");
-console.log(intersection(setA, setB)); // {}
-
-module.exports = { intersection };
+  }
+  
+  let setA = new Set(["Java", "Python", "Javascript", "C ++", "C#"]);
+  let setB = new Set(["Java", "Python"]);
+  console.log(intersection(setA, setB)); // {"Java", "Python"}
+  
+  setA = new Set("Java", "Python");
+  setB = new Set("Swift");
+  console.log(intersection(setA, setB)); // {}
+  
+  module.exports = { intersection };
